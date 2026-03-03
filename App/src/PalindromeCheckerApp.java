c3d0416754019cf93e241d83c21cc92cd99e0f88
sadhnasrinivasan2006 <sadhnasrinivasan2006@gmail.com>
2026-03-03 22:33:37 +0530

    UC13: Performance Comparison

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,8 +3,13 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String input = "racecar";
        PalindromeStrategy strategy=new StackStrategy();
+       long starttime=System.nanoTime();
        boolean isPalindrome=strategy.check(input);
-       System.out.print("is it a palindrome?:"+isPalindrome);
+       long endtime=System.nanoTime();
+       long executiontime=endtime-starttime;
+       System.out.println("input:"+input);
+       System.out.println("is it a palindrome?:"+isPalindrome);
+       System.out.println("execution time:"+executiontime+"ns");
     }
 
 }
